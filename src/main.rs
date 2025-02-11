7a6c888bb4f6fde1cc9755ff5e5a672a6ca0a12b
Suharjin <suharjin01@gmail.com>
2025-02-11 21:05:03 +0800

    Duration

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use chrono::{Datelike, NaiveDate};
+    use chrono::{Datelike, Duration, NaiveDate};
 
 
     #[test]
@@ -14,5 +14,16 @@ mod tests {
         println!("{}", date.year());
         println!("{}", date.month());
         println!("{}", date.day());
+    }
+
+    // Contoh Duration
+    #[test]
+    fn test_duration() {
+        let date = NaiveDate::from_ymd_opt(2025, 2, 11).unwrap();
+        let duration = Duration::days(10);
+        let new_date: NaiveDate = date + duration;
+
+        println!("{}", date);
+        println!("{}", new_date);
     }
 }
